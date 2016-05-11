e28a6b213afe82f9d750cab0ca51b5556f658b58
luisomoreau <luis.omoreau@gmail.com>
2016-05-11 10:44:43 +0200

    Feat - Added twits status depending on messages received

--- common/models/message.js
+++ common/models/message.js
@@ -1,36 +1,29 @@
 var Twitter = require('twitter');
 
 module.exports = function(Message) {
-  var app = require('../../server/server');
-
-  Message.observe('loaded', function(ctx, next) {
-    var Twit = app.models.RandomTwit;    // works!
-    //console.log(Twit);
-    Twit.find(function (err,data) {
-      if (err){
-        console.log(err)
-      }else{
-        console.log(data);
-        next();
-      }
-    })
-
-  });
-
-  var client = new Twitter({
-    consumer_key: 'HPYbuuolFJHKEWkaejpiDKBNf',
-    consumer_secret: 'fMTjv3HbEqumSPhOzCIOPgEwNfZ2ggVtJkPmPQWBoipoeA7ZJ4',
-    access_token_key: '728509639176884226-IKkFFqXqwKs395WLwmb5Pu0B7mG8krD',
-    access_token_secret: 'e0ZvUCqvt0as2z5j6vadwJmp6KOwAPanl6MnurEn6w8vc'
-  });
-
-  // Use the environment variables in production
-  // var client = new Twitter({
-  //   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  //   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  //   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  //   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  //var app = require('../../server/server');
+  // Message.observe('loaded', function(ctx, next) {
+  //   var Twit = app.models.RandomTwit;
+  //   //console.log(Twit);
+  //   Twit.find(function (err,data) {
+  //     if (err){
+  //       console.log(err)
+  //     }else{
+  //       //console.log(data);
+  //       statuses = data;
+  //       next();
+  //     }
+  //   })
+  //
   // });
+
+  //Use the environment variables in production
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
 
   //Available methods :
   // client.get(path, params, callback);
@@ -42,16 +35,43 @@ module.exports = function(Message) {
     console.log('time : '+message.time);
     console.log('device id : '+message.device);
     console.log('data : '+message.data);
+    console.log('temp : '+message.temp);
+    console.log('lum : '+message.lum);
+    console.log('hum : '+message.hum);
+    console.log('voltage : '+message.voltage);
+    var statuses = [];
+    if(message.temp<15){
+      statuses.push("I'm feeling a bit cold... "+message.temp+" °C. https://t.co/qblek65GFk");
+      statuses.push("It's freezing in here..."+message.temp+" °C. https://t.co/qblek65GFk");
 
-    var statuses = [
-      {"status": "Wow it's so hot in the office"+message.temp+"°C, can someone turn on the air conditioning system?"}
-    ]
+    }
+    if(message.hum<10){
+      statuses.push("Could anyone bring me water ? I'm below "+message.hum+" % of water. https://t.co/qblek65GFk");
+      statuses.push("Hey guys!! I'm starting to be a bit thirsty... I'm below "+message.hum+" % of water. https://t.co/qblek65GFk");
+    }
+    if(message.temp>28){
+      statuses.push("Wow it's hot in here... "+message.temp+" °C. https://t.co/qblek65GFk");
+      statuses.push("It's warm in the office "+message.temp+" °C. https://t.co/qblek65GFk");
+    }
+    var length = statuses.length;
+    console.log(length);
+    
+    if(length==0){
+      console.log("No corresponding status...")
+    }else{
+      var random = Math.floor(Math.random() * length);
+      console.log(random);
 
-    // client.post('statuses/update', {status: 'https://t.co/qblek65GFk'},  function(error, tweet, response){
-    //     if(error) console.log(error);
-    //     console.log(tweet);  // Tweet body.
-    //     console.log(response);  // Raw response object.
-    //   });
+      var status = statuses[random];
+      console.log(status);
+      client.post('statuses/update', {status: status},  function(error, tweet, response){
+          if(error) console.log(error);
+          console.log(tweet);  // Tweet body.
+          console.log(response);  // Raw response object.
+        });
+    }
+
+  
     next();
   });
 
